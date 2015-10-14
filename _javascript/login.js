3e2e8329e8639e3514c00ddde9c4f92ae3709df7
drizzutojr <drizzuto@smu.edu>
2015-10-14 00:04:16 -0500

    working on login.js

--- _javascript/login.js
+++ _javascript/login.js
@@ -1,6 +1,30 @@
-$("#sub").click( function() {
+$(document).ready(function() 
+{
+	$("#submitButton").click(function() 
+	{
 
-	$("#submit_button").click(funtion {
-		var name = $.(".login-form")
-	});
+		var emailLogin = $("#emailLogin").val();
+		var passwordLogin = $("#passLogin").val();
+
+		if(emailLogin=='' || passwordLogin == '') 
+		{
+			$('input[type="text"],input[type="password"]')
+				.css("border","2px solid red");
+			$('input[type="text"],input[type="password"]')
+				.css("box-shadow","0 0 3px red");
+			alert("Username or Password not correct, please try again.");
+		}
+		else 
+		{
+			$.post("../_api/login", 
+			{
+				email: emailLogin,
+				password: passwordLogin
+			}, 
+			function(data) 
+			{
+				alert(data);
+			});
+
+		}
 });
